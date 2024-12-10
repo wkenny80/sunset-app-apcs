c11aeff0db8cbbf617d9b0ab47763d35fea56298
Peletic <its.peletic@gmail.com>
2024-12-10 11:23:50 -0800

    complete needs of the actual functionality, still needs the functionality

--- sunset-app-apcs/src/app/page.tsx
+++ sunset-app-apcs/src/app/page.tsx
@@ -4,11 +4,14 @@ import LinkedAlertComponent from "@/components/variable/LinkedAlertComponent";
 import {Input} from "@/components/ui/input";
 import {Card, CardContent, CardHeader} from "@/components/ui/card";
 import {Button} from "@/components/ui/button";
+import {getWeather} from "@/lib/weatherUtils";
 
 export default function Home() {
     const [displayVariable, setDisplayVariable] = useState<string>("");
+    const [forecast, setForecast] = useState<string>("");
     const [clouds, setClouds] = useState<boolean>();
     const [humidity, setHumidity] = useState<number>(-1);
+    const [temp, setTemp] = useState<number>(-1);
     return (
         <div
             className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -28,9 +31,61 @@ export default function Home() {
             </main>
             <main className="flex flex-row gap-16 row-start-2 items-center sm:items-start">
                 <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                    <LinkedAlertComponent stateHeader={"Forecast"} state={forecast}/>
                     <LinkedAlertComponent stateHeader={"Clouds"} state={clouds}/>
                     <LinkedAlertComponent stateHeader={"Humidity"} state={humidity}/>
-
+                    <LinkedAlertComponent stateHeader={"Temp"} state={temp}/>
+                    <Button onClick={async () => {
+                        const weather = await getWeather();
+                        /*
+                        detailedForecast
+:
+"Mostly sunny. High near 65, with temperatures falling to around 60 in the afternoon. North northwest wind around 3 mph."
+endTime
+:
+"2024-12-10T18:00:00-08:00"
+icon
+:
+"https://api.weather.gov/icons/land/day/sct?size=medium"
+isDaytime
+:
+true
+name
+:
+"Today"
+number
+:
+1
+probabilityOfPrecipitation
+:
+{unitCode: 'wmoUnit:percent', value: null}
+shortForecast
+:
+"Mostly Sunny"
+startTime
+:
+"2024-12-10T11:00:00-08:00"
+temperature
+:
+65
+temperatureTrend
+:
+""
+temperatureUnit
+:
+"F"
+windDirection
+:
+"NNW"
+windSpeed
+:
+"3 mph"
+                         */
+                        console.log(weather)
+                        setForecast(weather.detailedForecast);
+                        setClouds(weather.shortForecast);
+                        setTemp(weather.temperature);
+                    }}>btn</Button>
                 </div>
             </main>
         </div>
--- sunset-app-apcs/src/lib/weatherUtils.ts
+++ sunset-app-apcs/src/lib/weatherUtils.ts
@@ -1,13 +1,14 @@
-import {Dispatch, SetStateAction} from "react";
+export const WEATHER_POINT_API_URL = "https://api.weather.gov/points/";
+export const WEATHER_API_URL = "https://api.weather.gov/gridpoints/";
+export const GEOLOCATION_API_URL = "https://ipwho.is/";
 
-export const WEATHER_API_URL = "https://api.weather.gov/";
-export const GEOLOCATION_API_URL = "https://ip-api.com/json/%IP%";
-export const IP_ADDRESS_API_URL = "https://api.ipify.org?format=json"
-export function getAndSetWeather(clouds : Dispatch<SetStateAction<any>>, humidity : Dispatch<SetStateAction<any>>) {
+export async function getWeather() : Promise<any> {
+    const geolocationRes : any = await (await fetch(GEOLOCATION_API_URL)).json();
+    const pointRes : any = await (await fetch(WEATHER_POINT_API_URL + `${geolocationRes.latitude + ',' + geolocationRes.longitude}`)).json();
+    const gridIdentifier = pointRes.properties.gridId;
+    const pointX = pointRes.properties.gridX;
+    const pointY = pointRes.properties.gridY;
 
-}
-export async function getWeather() : Promise<{ clouds: any, humidity: any }> {
-    const ipAddress : string = (await (await fetch(IP_ADDRESS_API_URL)).json()).ip;
-    const geolocationRes : any = await (await fetch(GEOLOCATION_API_URL.replace("%IP%", ipAddress))).json();
-    return {clouds: "a", humidity: "a"};
+    const weatherRes = await (await fetch(WEATHER_API_URL + `${gridIdentifier}/${pointX},${pointY}/forecast`)).json();
+    return weatherRes.properties.periods[0];
 }
