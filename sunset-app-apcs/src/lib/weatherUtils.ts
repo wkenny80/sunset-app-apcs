44ede9139c10e36cf1f8c5b3caec43fe3d438901
peletic <its.peletic@gmail.com>
2025-01-07 10:56:31 -0800

    deprecated api key

--- sunset-app-apcs/src/lib/weatherUtils.ts
+++ sunset-app-apcs/src/lib/weatherUtils.ts
@@ -1,7 +1,7 @@
 export const WEATHER_POINT_API_URL = "https://api.weather.gov/points/";
 export const WEATHER_API_URL = "https://api.weather.gov/gridpoints/";
 export const GEOLOCATION_API_URL = "https://ipwho.is/";
-export const HUMIDITY_API_URL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=dc97ae444550de95a8c38bd958408dd5"
+export const HUMIDITY_API_URL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=4aa906615a8dbd7c374c7f617de5396e"
 
 export async function getWeather() : Promise<any> {
     const geolocationRes : any = await (await fetch(GEOLOCATION_API_URL)).json();
